31a7ed4c43e8b94415f2d19272f81c7a74609b63
yatharth <yatharthmakvana@gmail.com>
2025-04-30 18:57:38 +0530

    Created grid with easier method for 16x16 grid and added the hover effect successfully.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function grid(size){
 let container=document.querySelector(".container");
-for(let i=0;i<size;i++){
-    let row=document.createElement("div");
-    row.classList.add("row");
-    row.style.border="2px solid black";
-    for(let j=0;j<=size;j++){
-        let column=document.createElement("div");
-        column.classList.add("column");
-        column.style.border="2px solid black";
-        row.appendChild(column);
-    }
-    container.appendChild(row);
+for(let i=0;i<size*size;i++){
+    let square= document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseenter",()=>{
+        square.classList.add("hovered");
+    })
+    container.appendChild(square);
 }
 }
 
 grid(16);
+
+const sq=document.querySelector(".square");
+
+
+
+
+
+
 
 
