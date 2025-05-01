dc076185a032a55c30a4912b6f9f71cb72559fe5
yatharth <yatharthmakvana@gmail.com>
2025-05-01 22:59:34 +0530

    Added the hover effect where mouse leave trails when passed through a square div and also Added the button to reset the grid and take the new grid size as input from the user.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function grid(size){
+let input=16;
 let container=document.querySelector(".container");
+
+function grid(size){
+
+
 for(let i=0;i<size*size;i++){
     let square= document.createElement("div");
     square.classList.add("square");
+    square.style.width= 600/size + "px";
     square.addEventListener("mouseenter",()=>{
         square.classList.add("hovered");
     })
@@ -10,7 +15,24 @@ for(let i=0;i<size*size;i++){
 }
 }
 
-grid(16);
+function cleanGrid(){
+   while(container.firstChild){
+    container.removeChild(container.firstChild);
+   }
+}
+
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",()=>{
+    let inp=parseInt(prompt("Enter Grid Size:"));
+    cleanGrid();
+    if(inp>100){
+        inp=100;
+    }
+    grid(inp);
+})
+
+grid(input);
 
 
 
