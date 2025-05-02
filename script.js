35c42eb9991ee51dfa56741d1f1537ed5b94a49d
yatharth <yatharthmakvana@gmail.com>
2025-05-03 01:06:22 +0530

    Added various checkboxes such as random color , shading and eraser while implementing their logic. Also made the project responsive for mobile devices.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let input=16;
 let container=document.querySelector(".container");
+let check=document.querySelector(".checkbox");
+let shade=document.querySelector(".shading");
+let eraser=document.querySelector(".eraser");
+
+let a=0.1;
 
 function grid(size){
 
@@ -8,9 +13,26 @@ for(let i=0;i<size*size;i++){
     let square= document.createElement("div");
     square.classList.add("square");
     square.style.width= 600/size + "px";
-    square.addEventListener("mouseenter",()=>{
-        square.classList.add("hovered");
+    square.addEventListener("mouseover",()=>{
+        if(check.checked){
+            let r=Math.floor(Math.random()*256);
+            let g=Math.floor(Math.random()*256);
+            let b=Math.floor(Math.random()*256);
+
+            square.style.backgroundColor=`rgb(${r},${g},${b})`;
+        }
+        else if(shade.checked){
+            a+=0.1;
+            square.style.backgroundColor=`rgba(${0},${0},${0},${a})`;
+        }
+        else if(eraser.checked){
+            square.style.backgroundColor="#bfbfbf"
+        }
+        else{
+            square.style.backgroundColor="black";
+        }
     })
+    square.style.width=`${100/size}%`;
     container.appendChild(square);
 }
 }
